346e42cdb149b62d480dcb83431f6b79c8a2487d
snowsphere <prismistim@hotmail.com>
2020-07-18 01:11:34 +0900

    add: トップページを作成 (WIP)

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,34 +27,21 @@
     <v-app-bar
       :clipped-left="clipped"
       fixed
+      dense
+      collapse
+      collapse-on-scroll
+      flat
       app
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn
         icon
-        @click.stop="rightDrawer = !rightDrawer"
+        href="https://github.com/prismistim/gen-of-miracles"
+        target="_blank"
       >
-        <v-icon>mdi-menu</v-icon>
+        <v-icon>mdi-github</v-icon>
       </v-btn>
     </v-app-bar>
     <v-content>
@@ -62,28 +49,11 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }} gen-of-miracles</span>
     </v-footer>
   </v-app>
 </template>
@@ -97,8 +67,8 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-home-variant',
+          title: 'Home',
           to: '/'
         },
         {
@@ -108,10 +78,16 @@ export default {
         }
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: '奇跡の世代'
     }
   }
 }
 </script>
+
+<style>
+/* @import url('https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;600&display=swap');
+
+.v-application {
+  font-family: "Noto Serif JP", serif;
+} */
+</style>
