8bd3fa048cf834f00606c0c78b78e9ab84aa0790
snowsphere <prismistim@hotmail.com>
2020-07-19 23:32:41 +0900

    add: 周囲の反応のコンポーネントを追加

---
+++ components/ReactionItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-col
+    cols="12"
+    lg="6"
+    md="6"
+    sm="6"
+  >
+    <v-skeleton-loader
+      :loading="loading"
+      type="list-item-avatar-three-line"
+    >
+      <v-card min-height="120">
+        <v-row>
+          <v-col
+            cols="12"
+          >
+            <div class="mb-2">
+              <v-avatar class="ml-4">
+                <img
+                  :src="'/images/avatar/' + name + '.jpg'"
+                >
+              </v-avatar>
+              <v-chip class="mx-2">
+                {{ name }}
+              </v-chip>
+            </div>
+            <div class="text-body-2 mx-4">
+              {{ description }}
+            </div>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-skeleton-loader>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface Data {
+  loading: boolean
+}
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  },
+  data (): Data {
+    return {
+      loading: true
+    }
+  },
+  mounted () {
+    this.loading = false
+  }
+})
+</script>
---
+++ components/Reactions.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container fluid>
+    <div>
+      <div class="text-h3 text-center mb-2">
+        周囲の反応
+      </div>
+      <div class="text-body-1 text-center">
+        奇跡の世代の活動は様々な場所で反響を呼び、その都度高い評価をいただいております。
+        その反応の一部を掲載させていただきます。
+      </div>
+    </div>
+    <v-row>
+      <reaction-item
+        v-for="item in reactions"
+        :key="item.name"
+        :name="item.name"
+        :description="item.description"
+      />
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ReactionItem from '@/components/ReactionItem.vue'
+const d : reactionData = require('@/assets/reactions.json')
+
+interface Reaction {
+  name: string,
+  description: string
+}
+
+interface reactionData {
+  [key: string]: Array<Reaction>
+}
+
+export default Vue.extend({
+  components: {
+    'reaction-item': ReactionItem
+  },
+  data () {
+    return {
+      reactions: d[1]
+    }
+  }
+})
+</script>
