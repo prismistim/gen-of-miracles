589b20f5f8312d3207fd7ea358e36642131dc493
snowsphere <prismistim@hotmail.com>
2020-08-17 02:47:15 +0900

    add: historyでjsonのデータをv-timelineに渡す処理を追加

---
+++ pages/history.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-timeline>
+      <v-timeline-item
+        v-for="item in historyData"
+        :key="item.date"
+      >
+        <v-card>
+          <v-card-subtitle>
+            {{ item.date }}
+          </v-card-subtitle>
+          <v-card-text>
+            <div
+              v-for="event in item.topic"
+              :key="event.summary"
+            >
+              {{ event.text }}<br>
+              {{ event.summary }}
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import historyDataJson from '@/assets/history.json'
+
+interface Topic {
+  summary: string,
+  text: string
+}
+
+interface Event {
+  date: string,
+  topic: Array<Topic>
+}
+
+interface History {
+  [key: string]: Array<Event>
+}
+
+export default Vue.extend({
+  computed: {
+    historyData: () => historyDataJson.history
+  }
+})
+</script>
