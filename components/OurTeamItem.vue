91913d5b2c8de53d53cca0a7370fb7e1a2bcc63f
snowsphere <prismistim@hotmail.com>
2020-08-26 23:24:29 +0900

    add: OurTeams追加

---
+++ components/OurTeamItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-col
+    cols="6"
+    md="3"
+    sm="6"
+  >
+    <v-card>
+      <v-img
+        :src="'/images/ours/' + name.toLowerCase() + '.jpg'"
+      />
+      <v-card-title>
+        {{ name }}
+      </v-card-title>
+      <v-card-subtitle>
+        {{ job }}
+      </v-card-subtitle>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    job: {
+      type: String,
+      default: null
+    }
+  }
+})
+</script>
---
+++ components/OurTeams.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-row>
+    <OurTeamItem
+      v-for="(item, i) in members"
+      :key="i"
+      :name="item.name"
+      :job="item.job"
+    />
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import OurTeamItem from '@/components/OurTeamItem.vue'
+
+export default Vue.extend({
+  components: {
+    OurTeamItem
+  },
+  computed: {
+    members: () => [
+      {
+        name: 'SOMA',
+        job: 'Leader'
+      },
+      {
+        name: 'TNK',
+        job: 'Developer'
+      },
+      {
+        name: 'MizukiCom',
+        job: 'Designer'
+      },
+      {
+        name: 'Wakame-Chan',
+        job: 'Dubtter'
+      }
+    ]
+  }
+})
+</script>
