37f3439bdbf280bfc7fa4c9f6d3e1a2411010b09
snowsphere <prismistim@hotmail.com>
2020-08-26 23:26:09 +0900

    add: メンバー紹介用画像コンポーネントの追加

---
+++ components/ImageGrids.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row class="mx-1">
+    <v-col cols="12" sm="4">
+      <v-card class="d-flex mb-2">
+        <v-img :src="`/images/members/${name}/1.jpg`" />
+      </v-card>
+      <v-card >
+        <v-img :src="`/images/members/${name}/2.jpg`" />
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-card>
+        <v-img :src="`/images/members/${name}/3.jpg`" />
+      </v-card>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-card class="mb-2">
+        <v-img :src="`/images/members/${name}/4.jpg`" />
+      </v-card>
+      <v-card>
+        <v-img :src="`/images/members/${name}/5.jpg`" />
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
